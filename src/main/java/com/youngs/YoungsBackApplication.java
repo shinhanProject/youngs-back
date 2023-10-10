bfef6d890ada1d911d97fd80a54561c9a3ff6898
C0012S <alwaysh1002@gmail.com>
2023-10-10 22:02:34 +0900

    [#1] init : project

---
+++ src/main/java/com/youngs/YoungsBackApplication.java
@@ -0,0 +1,13 @@
+package com.youngs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngs/YoungsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
