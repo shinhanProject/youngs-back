0cc04bea4a16c70c08072e1ea87fdab6093c3db8
C0012S <alwaysh1002@gmail.com>
2023-10-11 19:57:47 +0900

    [#1] init : ResponseDTO

---
+++ src/main/java/com/youngs/dto/ResponseDTO.java
@@ -0,0 +1,17 @@
+package com.youngs.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResponseDTO<T> {
+    private String message;
+    private List<T> data;
+}
