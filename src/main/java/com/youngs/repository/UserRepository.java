7c4db941f6c0052c4e5f93b7cb3b48daedd7bb4e
C0012S <alwaysh1002@gmail.com>
2023-10-13 13:56:08 +0900

    [#8] fix : UserRepository

--- src/main/java/com/youngs/repository/UserRepository.java
+++ src/main/java/com/youngs/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.youngs.repository;
 
 import com.youngs.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
