486786e48ea71532577d23c44a0c0319b0fd4dff
Jieun Lee <jieun98.dev@gmail.com>
2023-10-14 15:46:34 +0900

    [#16] feat : 팔로잉 목록 조회 API

--- src/main/java/com/youngs/controller/MyPageController.java
+++ src/main/java/com/youngs/controller/MyPageController.java
@@ -1,5 +1,6 @@
 package com.youngs.controller;
 
+import com.youngs.dto.FollowingDTO;
 import com.youngs.dto.ResponseDTO;
 import com.youngs.dto.UserSandDTO;
 import com.youngs.service.MyPageService;
@@ -27,7 +28,7 @@ public class MyPageController {
     public ResponseEntity<?> searchUserSand(@PathVariable Long userSeq){
         try{
             //사용자 인덱스에 해당하는 모래사장 조회
-            List<UserSandDTO> userSandList = myPageService.searchuserSandList(userSeq);
+            List<UserSandDTO> userSandList = myPageService.searchUserSandList(userSeq);
             return ResponseEntity.ok().body(userSandList);
         } catch (Exception e){
             ResponseDTO<Object> responseDTO = ResponseDTO.builder().message(e.getMessage()).build();
@@ -36,4 +37,25 @@ public class MyPageController {
                     .body(responseDTO);
         }
     }
+
+    /**
+     * 팔로잉 목록 조회 API
+     * @author 이지은
+     * @param userSeq 조회할 사용자 인덱스
+     * @return 팔로잉 리스트
+     * */
+    @GetMapping("/{userSeq}/following")
+    public ResponseEntity<?> searchFollowing(@PathVariable Long userSeq){
+        try{
+            //사용자 인덱스에 해당하는 팔로잉 목록 조회
+            List<FollowingDTO> followingDTOList = myPageService.sarchFollowingList(userSeq);
+            return ResponseEntity.ok().body(followingDTOList);
+        } catch (Exception e){
+            ResponseDTO<Object> responseDTO = ResponseDTO.builder().message(e.getMessage()).build();
+            return ResponseEntity
+                    .internalServerError() // 500
+                    .body(responseDTO);
+        }
+    }
+
 }
--- src/main/java/com/youngs/service/MyPageServiceImpl.java
+++ src/main/java/com/youngs/service/MyPageServiceImpl.java
@@ -1,7 +1,12 @@
 package com.youngs.service;
 
+import com.youngs.dto.FollowingDTO;
 import com.youngs.dto.UserSandDTO;
+import com.youngs.entity.Following;
+import com.youngs.entity.User;
 import com.youngs.entity.UserSand;
+import com.youngs.repository.FollowingRepository;
+import com.youngs.repository.UserRepository;
 import com.youngs.repository.UserSandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,8 @@ import java.util.List;
 @Transactional
 public class MyPageServiceImpl implements  MyPageService{
     private final UserSandRepository userSandRep;
+    private final FollowingRepository followingRep;
+    private final UserRepository userRep;
 
     /**
      * 모래사장 조회
@@ -25,7 +32,7 @@ public class MyPageServiceImpl implements  MyPageService{
      * @return 모래사장 데이터
      * */
     @Override
-    public List<UserSandDTO> searchuserSandList(Long userSeq) {
+    public List<UserSandDTO> searchUserSandList(Long userSeq) {
         List<UserSand> userSandList = userSandRep.findAllByUserUserSeq(userSeq);
         if(userSandList.isEmpty()){ //가져온 모래사장이 비어있다면
             throw new RuntimeException("조회할 모래사장 정보가 없습니다.");
@@ -38,4 +45,38 @@ public class MyPageServiceImpl implements  MyPageService{
         }
         return userSandDTOList;
     }
+
+    /**
+     * 팔로잉 조회
+     * @author 이지은
+     * @param userSeq 사용자 인덱스
+     * @exception RuntimeException 가져온 팔로잉 목록 정보가 없다면 throw
+     * @return 팔로잉 목록
+     * */
+    @Override
+    public List<FollowingDTO> sarchFollowingList(Long userSeq){
+        List<Following> followingList = followingRep.findAllByFollowerUserSeq(userSeq);
+        if(followingList.isEmpty()){
+            throw new RuntimeException("조회할 팔로잉 목록이 없습니다.");
+        }
+        List<FollowingDTO> followingDTOList = new ArrayList<>();
+        for(Following f : followingList){
+            System.out.println("f: " + f);
+            //팔로우 리스트로 불러온 사용자의 정보 조회
+            User user = userRep.findByUserSeq(f.getFollowing().getUserSeq());
+
+            int status = 0; //default: 자기 자신일 때
+            if(userSeq != null) { //로그인을 한 유저일 때
+                if (!userSeq.equals(user.getUserSeq())) { //클릭한 타겟이 본인이 아닐 때
+                    Following following = followingRep.findByFollowerAndAndFollowing(userSeq, user.getUserSeq());
+                    status = (following != null ? 2 : 1); //팔로잉 중이라면 2, 아니라면 1
+                }
+            } else { //로그인을 하지 않은 유저일 때
+                status = 1;
+            }
+            followingDTOList.add(new FollowingDTO(user.getNickname(), user.getProfile(), status));
+        }
+        return followingDTOList;
+    }
+
 }
