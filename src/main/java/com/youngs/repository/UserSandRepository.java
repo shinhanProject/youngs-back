37fd1eaab96865e6f4faa8fc6305dc8ad63f4d50
Jieun Lee <jieun98.dev@gmail.com>
2023-10-14 18:36:21 +0900

    [#16] feat : 사용자 프로필 정보 조회 API

--- src/main/java/com/youngs/repository/UserSandRepository.java
+++ src/main/java/com/youngs/repository/UserSandRepository.java
@@ -2,8 +2,13 @@ package com.youngs.repository;
 
 import com.youngs.entity.UserSand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface UserSandRepository extends JpaRepository<UserSand, Integer> {
     List<UserSand> findAllByUserUserSeq(Long userSeq);
+
+    @Query(value = "select sand.* from (select count(*) from user_sand u where user_seq=?1) sand", nativeQuery = true)
+    int getByCountAndUserUserSeq(Long userSeq);
 }
