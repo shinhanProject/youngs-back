262435c489a99954df052d4245e3ca7fa0f8d37e
C0012S <alwaysh1002@gmail.com>
2023-10-16 15:13:12 +0900

    [#45] refactor : 요약 작성 시 이미 작성되어 있는 요약이 있을 경우 예외 처리

--- src/main/java/com/youngs/service/SummaryServiceImpl.java
+++ src/main/java/com/youngs/service/SummaryServiceImpl.java
@@ -31,13 +31,18 @@ public class SummaryServiceImpl implements SummaryService {
      * @param userSeq : 요약을 작성할 사용자의 고유 번호
      * @param summaryDTO : 작성할 요약 정보
      * @return - 200 : 기초 지식 요약 작성 성공
-     * @return - 500 : 기초 지식 요약 작성 실패
+     * @return - 500 : 기초 지식 요약 작성 실패, 이미 기초 지식 요약이 작성되어 있을 경우
      **/
     @Override
     public ResponseEntity<?> writeBasicSummary(Long userSeq, SummaryDTO summaryDTO) {
         try {
-            User user = userRepository.findByUserSeq(userSeq);
-            BasicArticle basicArticle = basicRepository.findByBasicSeq(summaryDTO.getArticleId());
+            Long basicSeq = summaryDTO.getArticleId();
+            if (basicSummaryRepository.existsById(basicSeq)) { // 이미 요약 작성이 되어 있을 경우
+                throw new RuntimeException("이미 기초 지식 요약이 작성되어 있습니다.");
+            }
+
+            User user = userRepository.findByUserSeq(userSeq); // 요약을 작성할 사용자
+            BasicArticle basicArticle = basicRepository.findByBasicSeq(basicSeq); // 요약을 작성할 기초 지식 글
 
             BasicSummary basicSummary = BasicSummary.builder()
                     .context(summaryDTO.getContext())
@@ -45,7 +50,7 @@ public class SummaryServiceImpl implements SummaryService {
                     .basicArticle(basicArticle)
                     .build();
 
-            basicSummaryRepository.save(basicSummary);
+            basicSummaryRepository.save(basicSummary); // 기초 지식 요약 작성
 
             return ResponseEntity.ok().body(basicSummary);
         }
@@ -63,13 +68,18 @@ public class SummaryServiceImpl implements SummaryService {
      * @param userSeq : 요약을 작성할 사용자의 고유 번호
      * @param summaryDTO : 작성할 요약 정보
      * @return - 200 : 보도자료 요약 작성 성공
-     * @return - 500 : 보도자료 요약 작성 실패
+     * @return - 500 : 보도자료 요약 작성 실패, 이미 보도자료 요약이 작성되어 있을 경우
      **/
     @Override
     public ResponseEntity<?> writeNewsSummary(Long userSeq, SummaryDTO summaryDTO) {
         try {
-            User user = userRepository.findByUserSeq(userSeq);
-            NewsArticle newsArticle = newsRepository.findByNewsSeq(summaryDTO.getArticleId());
+            Long newsSeq = summaryDTO.getArticleId(); // 요약을 작성할 보도자료 고유 번호
+            if (newsSummaryRepository.existsById(newsSeq)) { // 이미 요약 작성이 되어 있을 경우
+                throw new RuntimeException("이미 보도자료 요약이 작성되어 있습니다.");
+            }
+
+            User user = userRepository.findByUserSeq(userSeq); // 요약을 작성할 사용자
+            NewsArticle newsArticle = newsRepository.findByNewsSeq(newsSeq); // 요약을 작성할 보도자료 글
 
             NewsSummary newsSummary = NewsSummary.builder()
                     .context(summaryDTO.getContext())
@@ -77,7 +87,7 @@ public class SummaryServiceImpl implements SummaryService {
                     .newsArticle(newsArticle)
                     .build();
 
-            newsSummaryRepository.save(newsSummary);
+            newsSummaryRepository.save(newsSummary); // 보도자료 요약 작성
 
             return ResponseEntity.ok().body(newsSummary);
         }
