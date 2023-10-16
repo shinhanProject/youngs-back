2d21baaad098f8087f9e81d8a456bb98253b8476
C0012S <alwaysh1002@gmail.com>
2023-10-17 03:03:49 +0900

    [#45] fix : 해당 사용자의 요약 수정

--- src/main/java/com/youngs/repository/BasicSummaryRepository.java
+++ src/main/java/com/youngs/repository/BasicSummaryRepository.java
@@ -12,4 +12,13 @@ public interface BasicSummaryRepository extends JpaRepository<BasicSummary, Long
      * @return 기초 지식 요약
      **/
     BasicSummary findByUserUserSeqAndBasicArticleBasicSeq(Long userSeq, Long basicSeq);
+
+    /**
+     * 사용자 고유 번호와 기초 지식 요약 고유 번호로 기초 지식 요약 조회
+     * @author : 박상희
+     * @param userSeq : 사용자 고유 번호
+     * @param summarySeq : 기초 지식 요약 고유 번호
+     * @return 기초 지식 요약
+     **/
+    BasicSummary findByUserUserSeqAndSummmarySeq(Long userSeq, Long summarySeq);
 }
