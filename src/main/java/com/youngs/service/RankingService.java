4d3ab2f433e4f555a9beb8fa20c1d4ba94760ce0
Jieun Lee <jieun98.dev@gmail.com>
2023-10-16 21:47:28 +0900

    [#8] fix: 로그인 여부에 따른 랭킹 조회

--- src/main/java/com/youngs/service/RankingService.java
+++ src/main/java/com/youngs/service/RankingService.java
@@ -1,9 +1,10 @@
 package com.youngs.service;
 
 import com.youngs.dto.UserRankDTO;
+import com.youngs.security.PrincipalUserDetails;
 
 import java.util.List;
 
 public interface RankingService {
-    List<UserRankDTO> getTop30UsersByPoint (Long userSeq);
+    List<UserRankDTO> getTop30UsersByPoint (PrincipalUserDetails currentUserDetails);
 }
