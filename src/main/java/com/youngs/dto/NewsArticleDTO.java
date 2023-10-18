cbb5eb1da3974e523ee1573f980af6392a4fe563
Jieun Lee <jieun98.dev@gmail.com>
2023-10-18 13:13:25 +0900

    [#4] etc: 세부 보도자료 조회시 반환 값 추가

--- src/main/java/com/youngs/dto/NewsArticleDTO.java
+++ src/main/java/com/youngs/dto/NewsArticleDTO.java
@@ -12,4 +12,21 @@ public class NewsArticleDTO {
     private String url; //보도자료 URL
     private String description; //보도자료 요약
     private String pubDate; //보도자료 게시일
+    private boolean wasWritten; // 요약 작성 여부
+
+    public NewsArticleDTO(Long newsSeq, String title, String url, String pubDate, boolean wasWritten){
+        this.newsSeq = newsSeq;
+        this.title = title;
+        this.url = url;
+        this.pubDate = pubDate;
+        this.wasWritten = wasWritten;
+    }
+
+    public NewsArticleDTO(Long newsSeq, String title, String url, String description, String pubDate){
+        this.newsSeq = newsSeq;
+        this.title = title;
+        this.url = url;
+        this.description = description;
+        this.pubDate = pubDate;
+    }
 }
