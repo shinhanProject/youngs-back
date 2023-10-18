cd907f0bd492d809de4672f196105158f2849ab9
haechi <epiclog@naver.com>
2023-10-19 05:36:26 +0900

    [#62] feat : 종목 세부 데이터 제공 기능

--- src/main/java/com/youngs/service/StockService.java
+++ src/main/java/com/youngs/service/StockService.java
@@ -25,6 +25,7 @@ public class StockService {
         String target_date = today.minusDays(1).format(DateTimeFormatter.ofPattern("YYYY-MM-dd"));
         int currentYear = today.getYear();
 
+
         // 종목 리스트 전체 - 이름, code
         List<ArrayList<String>> stockList = stockCodeRepository.findAllList();
         List<Map<String, Object>> dataList = new ArrayList<>();
@@ -55,6 +56,7 @@ public class StockService {
         return dataList;
     }
 
+    @Transactional
     public List<Map<String, Object>> getTodayStock() {
         LocalDate today = LocalDate.now();
         String target_date = today.format(DateTimeFormatter.ofPattern("YYYY-MM-dd"));
@@ -97,5 +99,68 @@ public class StockService {
         });
 
         return dataList.subList(0, 3);
+    }
+
+    @Transactional
+    public Map<String, Object> getStockStat(String corp_code) {
+        // corp_code로 재무제표 데이터 받아오기\
+        LocalDate today = LocalDate.now();
+        int year = today.getYear();
+        int lastYear = today.minusYears(1).getYear();
+        int month = today.getMonthValue();
+        Map<String, Object> stockData = new HashMap<>();
+
+        String key = corp_code + "/" + lastYear + "-" + month + ".json"; // s3 키 파일 값
+        String t_key = corp_code + "/totaldata/" + year + "-" + month + ".json";
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            InputStream objectContent = s3Service.downloadObject(key);
+            InputStream t_objectContent = s3Service.downloadObject(t_key);
+
+            JsonNode jsonData = objectMapper.readTree(objectContent);
+            JsonNode t_jsonData = objectMapper.readTree(t_objectContent);
+
+            // ------- 기본 데이터 -------
+            // 당기순이익 - netProfit
+            if (jsonData.has("당기순이익(손실)")) {
+                stockData.put("netProfit", "-" + jsonData.get("당기순이익(손실)").get("thstrm_amount"));
+            } else {
+                stockData.put("netProfit", jsonData.get("당기순이익").get("thstrm_amount"));
+            }
+            // 자본총계: totalEquity
+            stockData.put("totalEquity", jsonData.get("자본총계").get("thstrm_amount"));
+            //  자산총계: totalAssets
+            stockData.put("totalAssets", jsonData.get("자산총계").get("thstrm_amount"));
+            //  부채총계: totalLiabilities
+            stockData.put("totalLiabilities", jsonData.get("부채총계").get("thstrm_amount"));
+            // 유동자산: currentAssets
+            stockData.put("currentAssets", jsonData.get("유동자산").get("thstrm_amount"));
+            // 유동부채: currentLiabilities
+            stockData.put("currentLiabilities", jsonData.get("유동부채").get("thstrm_amount"));
+            // 영업이익: operatingProfit
+            if (jsonData.has("영업이익(손실)")) {
+                stockData.put("operatingProfit", "-" + jsonData.get("영업이익(손실)").get("thstrm_amount"));
+            } else {
+                stockData.put("operatingProfit", "" + jsonData.get("영업이익").get("thstrm_amount"));
+            }
+            // 매출액: revenue
+            if (jsonData.has("매출액")) {
+                stockData.put("revenue", jsonData.get("매출액").get("thstrm_amount"));
+            } else {
+                stockData.put("revenue", jsonData.get("수익(매출액)").get("thstrm_amount"));
+            }
+            // 배당금지급: dividendsPaid
+            if (jsonData.has("배당금지급")) {
+                stockData.put("dividendsPaid", jsonData.get("배당금지급").get("thstrm_amount"));
+            }else {
+                stockData.put("dividendsPaid", jsonData.get("배당금의 지급").get("thstrm_amount"));
+            }
+            // 시가총액: marketCap
+            stockData.put("marketCap", t_jsonData.get("시가총액"));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return stockData;
     }
 }
