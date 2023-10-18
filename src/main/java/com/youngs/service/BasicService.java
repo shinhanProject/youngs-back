dc3779af5dc76c451fdca14e79608e5a89e8c6ce
Jieun Lee <jieun98.dev@gmail.com>
2023-10-18 11:13:59 +0900

    [#31] etc : 기초 지식 게시물 조회 시 반환 값 추가

--- src/main/java/com/youngs/service/BasicService.java
+++ src/main/java/com/youngs/service/BasicService.java
@@ -2,11 +2,12 @@ package com.youngs.service;
 
 import com.youngs.dto.BasicArticleDTO;
 import com.youngs.security.PrincipalUserDetails;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface BasicService {
     List<BasicArticleDTO> searchBasicArticleList(PrincipalUserDetails currentUserDetails, Long categorySeq, boolean isChecked);
 
-    BasicArticleDTO searchBasicArticle(Long categorySeq, Long basicSeq);
+    ResponseEntity<?> searchBasicArticle(PrincipalUserDetails currentUserDetails, Long categorySeq, Long basicSeq);
 }
