c68e08de0ae4779a605439bbe7b5f4236119baaa
Jieun Lee <jieun98.dev@gmail.com>
2023-10-18 19:05:07 +0900

    [#80] feat: 포인트에 따른 Tier 값 변경 추가

---
+++ src/main/java/com/youngs/repository/TierRepository.java
@@ -0,0 +1,7 @@
+package com.youngs.repository;
+
+import com.youngs.entity.Tier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TierRepository extends JpaRepository<Tier, Long> {
+}
